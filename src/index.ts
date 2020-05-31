f63e2a8e315ba3cd5b6adad31368206a395026c2
Yasuo Saito <saito_ya@works-hi.co.jp>
2020-05-31 22:29:49 +0900

    add modal

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,10 @@ const app = new App({
 // ------------------------
 // Application Logic
 // ------------------------
-app.command("/message", async ({ ack, body, payload, context }) => {
+app.command("/message", async ({ ack, payload, context }) => {
     await ack();
 
+    // declare blocks for message
     const blocks: Block[] = [
         section({ text: mrkdwnText('*Where should we order lunch from?* Poll by <fakeLink.toUser.com|Mark>')}),
         divider(),
@@ -37,8 +38,9 @@ app.command("/message", async ({ ack, body, payload, context }) => {
         }),
         divider(),
         actions([ button(plainText('Add a suggestion'), 'add-suggestion', { value: 'click_me_123' })])
-    ]
+    ];
 
+    // post message
     await app.client.chat.postMessage({
         token: context.botToken,
         channel: payload.channel_id,
@@ -46,6 +48,50 @@ app.command("/message", async ({ ack, body, payload, context }) => {
         blocks: blocks
     });
 });
+
+app.command("/modal", async ({ ack, payload, context }) => {
+    await ack();
+
+    // declare blocks for modal
+    const blocks: Block[] = [
+        section({ text: mrkdwnText('*Hi <fakelink.toUser.com|@David>!* Here\'s how I can help you:')}),
+        divider(),
+        section({
+            text: mrkdwnText(':calendar: *Create event*\nCreate a new event'),
+            accessory: button(plainText('Create event'), 'create', { value: 'click_me_123', style: 'primary' })
+        }),
+        section({
+            text: mrkdwnText(':clipboard: *List of events*\nChoose from different event lists'),
+            accessory: staticSelect('chose-list', plainText('Choose list'), [
+                option(plainText('My events'), 'value-0'),
+                option(plainText('All events'), 'value-1'),
+                option(plainText('Event invites'), 'value-2')
+            ])
+        }),
+        section({
+            text: mrkdwnText(':gear: *Settings*\nManage your notifications and team settings'),
+            accessory: staticSelect('edit-settings', plainText('Edit settings'), [
+                option(plainText('Notifications'), 'value-0'),
+                option(plainText('Team settings'), 'value-1')
+            ])
+        }),
+        actions([
+            button(plainText('Send feedback'), 'send-feedback', { value: 'click_me_123' }),
+            button(plainText('FAQs'), 'faqs', { value: 'click_me_123' })
+        ]),
+    ];
+
+    // build modal
+    const modal = new Modal(plainText('App menu'), blocks, { close: plainText('Cancel'), submit: plainText('Submit')});
+
+    // open modal
+    await app.client.views.open({
+        token: context.botToken,
+        trigger_id: payload.trigger_id,
+        view: modal.getView()
+    });
+});
+
 
 
 // Start app
